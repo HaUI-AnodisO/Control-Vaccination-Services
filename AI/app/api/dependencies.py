ed393c73606de7b79c29d98df724711fb2ffb94f
Anodis108 <dong10082003@gmail.com>
2024-12-07 11:34:40 +0700

    refactor(structure): delete unnecessary file

--- AI/app/api/dependencies.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Depends
-from app.services.gemini_service import load_prompt
-
-# Dependency để lấy nội dung từ prompt.txt
-def get_prompt_content() -> str:
-    return load_prompt()
--- AI/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.api.routes import router
-
-# Initialize FastAPI app
-app = FastAPI()
-
-# Include API routes
-app.include_router(router)
